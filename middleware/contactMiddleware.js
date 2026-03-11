692d1d32ae62a7893d4128f2b15a5b09f06e9132
Shanbel Dires <shanbeldires7@gmail.com>
2026-03-11 15:35:27 +0300

    Update contactMiddleware.js

--- middleware/contactMiddleware.js
+++ middleware/contactMiddleware.js
@@ -3,6 +3,6 @@ const contactValidation = joi.object({
     name: joi.string().required().min(6),
     email: joi.string().email().required().min(6),
     adress: joi.string().required().min(6),
-    phone: joi.string().length(10)
+    phone: joi.string().min(10).required(),
 })
 export default contactValidation;
